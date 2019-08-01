2d3723da0f10f5014a37b4992ee5e839b88dc174
smluv82 <smluv82@naver.com>
2019-08-02 06:25:40 +0900

    test case 추가

--- src/test/java/me/ecology/test/UserTest.java
+++ src/test/java/me/ecology/test/UserTest.java
@@ -1,5 +1,7 @@
 package me.ecology.test;
 
+import static org.junit.Assert.assertNotNull;
+
 import javax.annotation.Resource;
 
 import org.junit.Test;
@@ -8,6 +10,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import me.ecology.app.user.service.UserService;
+import me.ecology.vo.user.User;
+import me.ecology.vo.user.UserParam;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -17,16 +21,40 @@ public class UserTest {
 
 	@Test
 	public void 계정_생성() throws Exception {
+		UserParam userParam = new UserParam();
+		userParam.setUserId("test2");
+		userParam.setUserPwd("12Qwaszx!@");
+		System.out.println("userParam : " + userParam);
 
+		User resUser = userService.signup(userParam);
+		System.out.println("resUser : " + resUser);
+
+		assertNotNull(resUser);
 	}
 
 	@Test
 	public void 계정_인증() throws Exception {
+		UserParam userParam = new UserParam();
+		userParam.setUserId("test1");
+		userParam.setUserPwd("12Qwaszx!@");
+		System.out.println("userParam : " + userParam);
 
+		User resUser = userService.signin(userParam);
+		System.out.println("resUser : " + resUser);
+
+		assertNotNull(resUser);
+		assertNotNull(resUser.getToken());
 	}
 
 	@Test
 	public void 토큰_refresh() throws Exception {
+		String userId = "test1";
+		System.out.println("userId : " + userId);
 
+		User resUser = userService.refresh(userId);
+		System.out.println("resUser : " + resUser);
+
+		assertNotNull(resUser);
+		assertNotNull(resUser.getToken());
 	}
 }
